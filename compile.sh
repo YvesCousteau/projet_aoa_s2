332edd66b6e871f0595e520d9d435bab3198536a
TERRUSS <olivier.benaben@protonmail.com>
2021-03-12 14:20:13 +0100

    nbsp err

--- compile.sh
+++ compile.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 make OPT=ORIGINAL
-for f in `ls | grep ^s13_`; do mv $f ORIGINAL_$f; done
+for f in `ls | grep ^s13_`; do mv $f ORIGINAL_$f; done
 
 make OPT=CORRECTED
-for f in `ls | grep ^s13_`; do mv $f CORRECTED_$f; done
+for f in `ls | grep ^s13_`; do mv $f CORRECTED_$f; done
