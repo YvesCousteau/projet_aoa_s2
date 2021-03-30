61c6475d86c159a2c8aedab2dd8584060aa113c3
Maxime Vincent <maxime.vincent@ens.uvsq.fr>
2021-03-30 17:11:39 +0200

    premegre

--- scripts/set_rep.sh
+++ scripts/set_rep.sh
@@ -1,14 +1,17 @@
-cd ../src/
-make clean;
+[ -z $1 ] && exit
 
+
+SRC="../src/"
+
+#make -C $SRC clean;
 CORE_ID=3 # the core id on which the bench is executed
 sudo cpupower -c $CORE_ID frequency-set --governor powersave
+make -C $SRC OPT=ORIGINAL;
 
-make OPT=CORRECTED2;
-
-CACHE=226
+CACHE=$1
 WARMUP=0
-REP=10
+REP=200
 
-taskset -c $CORE_ID ./s13_02 $CACHE $WARMUP $REP
-cd ../scripts/
+taskset -c $CORE_ID ${SRC}s13_02 $CACHE $WARMUP $REP > find_warmup.data
+gnuplot ../template.gp > RAM_REP.png
+eog RAM_REP.png
