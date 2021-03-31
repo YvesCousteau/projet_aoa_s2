921350fa4d14dd26f9d5aec5a97c6e17fef04f35
hugo-henrotte <hugo.henrotte@ens.uvsq.fr>
2021-03-31 11:39:50 +0200

    L2 warm size rep

--- scripts/set_rep.sh
+++ scripts/set_rep.sh
@@ -5,16 +5,18 @@ CORE_ID=3 # the core id on which the bench is executed
 sudo cpupower -c $CORE_ID frequency-set --governor powersave
 
 make OPT=ORIGINAL;
-#cd ../scripts/
+
 
 SIZE=159
 WARMUP=25
-REP=1
+REP=1000
 
 mkdir -p ../output
 for executable in `find . -type f -executable -name "s13*" `; do
   filename=$(basename $executable)
 
-  taskset -c $CORE_ID ../src/$filename $SIZE $WARMUP $REP >  ../output/${filename}.xlsx
+  # taskset -c $CORE_ID ./$filename $SIZE $WARMUP $REP >  ../output/${filename}.xlsx
+  taskset -c $CORE_ID ./$filename $SIZE $WARMUP $REP
 
 done
+cd ../scripts/
