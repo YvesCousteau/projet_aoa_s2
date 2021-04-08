6b7611a4b482fd4579da189feebe789633dfce0d
hugo-henrotte <hugo.henrotte@ens.uvsq.fr>
2021-04-08 13:40:40 +0200

    l2

--- src/kernel.c
+++ src/kernel.c
@@ -303,6 +303,122 @@ void s13 (unsigned n, const float a[n], const float b[n], float c[n][n], int off
       c [ i ][ j ] = a[j] < radius ? 0.0 : a [ j ] / bi;
     }
   }
+}
+
+#elif PARALLELE_ORIGINAL
+
+void s13 (unsigned n , const float a[n] ,const float b[n] , float c[n][n] ,int offset , double radius)
+{
+   int i, j;
+   for ( i =0; i < n ; i ++)
+   {
+     #pragma omp parallel for 
+       for ( j =0; j < n ; j ++)
+       {
+           if ( offset + j < 0 || offset + j >= n )
+              continue;
+           c [ i ][ offset + j ] = 0.0;
+           if ( a[offset + j] < radius )
+           {
+             c [ i ][ offset + j ] = a [ offset + j ] / b [ i ];
+           }
+        }
+   }
+}
+
+#elif defined PARALLELE_UNROLL_LOOPINVARIANT
+
+void s13 (unsigned n, const float a[n], const float b[n], float c[n][n], int offset, double radius) {
+	int i, j;
+
+  float bi;
+  float r = (double)radius;
+
+  for ( i = 0; i < n ; i ++)
+  {
+    bi = b [ i ];
+    #pragma omp parallel for
+    {
+      for ( j = offset; j < (n-3); j +=4) {
+        c [ i ][ j ] = a[j] < r ? 0.0 : a [ j ] / bi;
+        c [ i ][ j+1 ] = a[j+1] < r ? 0.0 : a [ j+1 ] / bi;
+        c [ i ][ j+2 ] = a[j+2] < r ? 0.0 : a [ j+2 ] / bi;
+        c [ i ][ j+3 ] = a[j+3] < r ? 0.0 : a [ j+3 ] / bi;
+      }
+
+      for(j = (n-3); j<n;j++){
+        c [ i ][ j ] = a[j] < r ? 0.0 : a [ j ] / bi;
+      }
+    }
+  }
+
+}
+
+#elif defined PARALLELE_UNROLL4_LOOPINVARIANT3
+
+void s13 (unsigned n, const float a[n], const float b[n], float c[n][n], int offset, double radius) {
+  int i, j;
+
+  float bi[4][4];
+
+  float r = (float)radius;
+
+  for ( i = 0; i < (n-3) ; i+= 4) {
+    bi[0][0] = b[i];bi[0][1] = b[i];bi[0][2] = b[i];bi[0][3] = b[i];
+    bi[1][0] = b[i+1];bi[1][1] = b[i+1];bi[1][2] = b[i+1];bi[1][3] = b[i+1];
+    bi[2][0] = b[i+2];bi[2][1] = b[i+2];bi[2][2] = b[i+2];bi[2][3] = b[i+2];
+    bi[3][0] = b[i+3];bi[3][1] = b[i+3];bi[3][2] = b[i+3];bi[3][3] = b[i+3];
+
+    #pragma omp parallel for
+    {
+      for ( j = offset; j < (n-3); j +=4) {
+
+        c [ i ][ j ] =   a[j] < r ? 0.0 :   a[j]  / bi[0][0];
+        c [ i ][ j+1 ] = a[j+1] < r ? 0.0 : a[j+1] / bi[0][1];
+        c [ i ][ j+2 ] = a[j+2] < r ? 0.0 : a[j+2] / bi[0][2];
+        c [ i ][ j+3 ] = a[j+3] < r ? 0.0 : a[j+3] / bi[0][3];
+
+        c [ i+1 ][ j ]   = a[j]  <  r ? 0.0 : a[j]  / bi[1][0];
+        c [ i+1 ][ j+1 ] = a[j+1] < r ? 0.0 : a[j+1] / bi[1][1];
+        c [ i+1 ][ j+2 ] = a[j+2] < r ? 0.0 : a[j+2] / bi[1][2];
+        c [ i+1 ][ j+3 ] = a[j+3] < r ? 0.0 : a[j+3] / bi[1][3];
+
+        c [ i+2 ][ j ] =   a[j]  <  r ? 0.0 : a[j]  / bi[2][0];
+        c [ i+2 ][ j+1 ] = a[j+1] < r ? 0.0 : a[j+1] / bi[2][1];
+        c [ i+2 ][ j+2 ] = a[j+2] < r ? 0.0 : a[j+2] / bi[2][2];
+        c [ i+2 ][ j+3 ] = a[j+3] < r ? 0.0 : a[j+3] / bi[2][3];
+
+        c [ i+3 ][ j ] =   a[j]  <  r ? 0.0 : a[j]  / bi[3][0];
+        c [ i+3 ][ j+1 ] = a[j+1] < r ? 0.0 : a[j+1] / bi[3][1];
+        c [ i+3 ][ j+2 ] = a[j+2] < r ? 0.0 : a[j+2] / bi[3][2];
+        c [ i+3 ][ j+3 ] = a[j+3] < r ? 0.0 : a[j+3] / bi[3][3];
+      }
+
+
+      for(j = (n-3); j<n;j++){
+        c [ i ][ j ]   = a[j] < r ? 0.0 : a[j]  / bi[0][0];
+        c [ i+1 ][ j ] = a[j] < r ? 0.0 : a[j] / bi[1][0];
+        c [ i+2 ][ j ] = a[j] < r ? 0.0 : a[j] / bi[2][0];
+        c [ i+3 ][ j ] = a[j] < r ? 0.0 : a[j] / bi[3][0];
+      }
+    }
+
+  }
+  for(i = (n-3); i<n;i++){
+    bi[0][0] = b[i];bi[0][1] = b[i];bi[0][2] = b[i];bi[0][3] = b[i];
+
+    for ( j = offset; j < (n-3); j +=4) {
+
+      c [ i ][ j ] =   a[j]  <  r ? 0.0 :  a[j]  / bi[0][0];
+      c [ i ][ j+1 ] = a[j+1] < r ? 0.0 : a[j+1] / bi[0][1];
+      c [ i ][ j+2 ] = a[j+2] < r ? 0.0 : a[j+2] / bi[0][2];
+      c [ i ][ j+3 ] = a[j+3] < r ? 0.0 : a[j+3] / bi[0][3];
+    }
+
+    for(j = (n-3); j<n;j++){
+      c [ i ][ j ]   = a[j] < r ? 0.0 : a[j] / bi[0][0];
+    }
+  }
 
 }
 
