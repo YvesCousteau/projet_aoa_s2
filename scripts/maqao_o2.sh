a5e8c951a0661b292f95b6b0e6469db0adffb829
TERRUSS <olivier.benaben@protonmail.com>
2021-04-10 11:05:55 +0200

    kopazeko

--- scripts/maqao_o2.sh
+++ scripts/maqao_o2.sh
@@ -10,5 +10,5 @@ make OPT=UNROLL_LOOPINVARIANT
 maqao oneview -R1 xp=../maqao_html/gcc02OUNROLL_LOOPINVARIANT -- ./s13_gcc_O2 51 30 750
 
 make clean
-make OPT=UNROLL_LOOPINVARIANT3
+make OPT=UNROLL4_LOOPINVARIANT3
 maqao oneview -R1 xp=../maqao_html/gcc02OUNROLL_LOOPINVARIANT3 -- ./s13_gcc_O2 51 30 750
